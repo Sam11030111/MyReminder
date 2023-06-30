d93b632d3d11f8dc71661b6a7d784ff3885a40f4
李竑陞 <sam86110306@gmail.com>
2023-06-30 15:34:35 +0800

    Initial Commit

---
+++ MyReminder/MyReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReminderApp.swift
+//  MyReminder
+//
+//  Created by 李竑陞 on 2023/6/30.
+//
+
+import SwiftUI
+
+@main
+struct MyReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyReminder/Reminder/CoredataReminder.swift
@@ -0,0 +1,10 @@
+//
+//  CoredataReminder.swift
+//  MyReminder
+//
+//  Created by 李竑陞 on 2023/6/30.
+//
+
+import Foundation
+import CoreData
+
